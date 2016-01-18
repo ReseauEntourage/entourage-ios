2d80bb92a76d86d10c0b5f5908c952153d65e56a
Ciprian Habuc <ciprian.habuc@tecknoworks.com>
2016-01-18 15:35:41 +0200

    defined appOrangeColor in UIColor category

---
+++ entourage/Classes/Utils/UIColor+entourage.h
@@ -0,0 +1,15 @@
+//
+//  UIColor+entourage.h
+//  entourage
+//
+//  Created by Ciprian Habuc on 18/01/16.
+//  Copyright © 2016 OCTO Technology. All rights reserved.
+//
+
+
+
+@interface UIColor (entourage)
+
+- (UIColor *)appOrangeColor;
+
+@end
---
+++ entourage/Classes/Utils/UIColor+entourage.m
@@ -0,0 +1,17 @@
+//
+//  UIColor+entourage.m
+//  entourage
+//
+//  Created by Ciprian Habuc on 18/01/16.
+//  Copyright © 2016 OCTO Technology. All rights reserved.
+//
+
+#import "UIColor+entourage.h"
+
+@implementation UIColor (entourage)
+
+- (UIColor *)appOrangeColor {
+    return [UIColor colorWithRed: 239.0f/255.0f green: 102.0f/255.0f blue:  47.0f/255.0f alpha:1.0f];
+}
+
+@end
