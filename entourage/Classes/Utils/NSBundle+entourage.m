351f6619fe9cd827531a1488e2da1cdb8eb9d115
CHip <ciprian.habuc@gmail.com>
2016-03-25 23:50:04 +0200

    send Application info on each start category on NSBundle for current version minor refactors

---
+++ entourage/Classes/Utils/NSBundle+entourage.m
@@ -0,0 +1,13 @@
+//
+//  NSBundle+entourage.m
+//  entourage
+//
+//  Created by Ciprian Habuc on 25/03/16.
+//  Copyright © 2016 OCTO Technology. All rights reserved.
+//
+
+#import "NSBundle+entourage.h"
+
+@implementation NSBundle (entourage)
+
+@end
