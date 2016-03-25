89c5941d5560610a97a4489b909e0cdd33ca4884
CHip <ciprian.habuc@gmail.com>
2016-03-25 23:50:04 +0200

    send Application info on each start category on NSBundle for current version fullscreen guide of POIs minor refactors

---
+++ entourage/Classes/Utils/NSBundle+entourage.h
@@ -0,0 +1,13 @@
+//
+//  NSBundle+entourage.h
+//  entourage
+//
+//  Created by Ciprian Habuc on 25/03/16.
+//  Copyright © 2016 OCTO Technology. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSBundle (entourage)
+
+@end
