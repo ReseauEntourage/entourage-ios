3f4681ab164c3efd72c4e157a77d9b08721b7dba
sergiu-buceac <sergiu.buceac@tecknoworks.com>
2016-08-11 17:37:39 +0300

    added filters for my entourages

---
+++ entourage/Classes/Controller/MyEntourages/OTMyEntouragesFiltersViewController.h
@@ -0,0 +1,13 @@
+//
+//  OTMyEntouragesFiltersViewController.h
+//  entourage
+//
+//  Created by sergiu buceac on 8/11/16.
+//  Copyright © 2016 OCTO Technology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OTMyEntouragesFiltersViewController : UIViewController
+
+@end
---
+++ entourage/Classes/Controller/MyEntourages/OTMyEntouragesFiltersViewController.m
@@ -0,0 +1,13 @@
+//
+//  OTMyEntouragesFiltersViewController.m
+//  entourage
+//
+//  Created by sergiu buceac on 8/11/16.
+//  Copyright © 2016 OCTO Technology. All rights reserved.
+//
+
+#import "OTMyEntouragesFiltersViewController.h"
+
+@implementation OTMyEntouragesFiltersViewController
+
+@end
