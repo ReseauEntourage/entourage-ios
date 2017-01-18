1424d109b85937422c943cc13f1186da42292a06
Sergiu Buceac <sergiu.buceac@tecknoworks.com>
2017-01-18 12:13:02 +0200

    EMA-962 add calls to update defualt partner

--- entourage/Classes/Controller/OTSelectAssociationViewController.m
+++ entourage/Classes/Controller/OTSelectAssociationViewController.m
@@ -58,7 +58,7 @@
 }
 
 - (void)saveAssociation {
-#warning TODO
+    [self.dataSource updateAssociation];
 }
 
 @end
--- entourage/Classes/Utils/Behaviors/UITableView/Associations/OTAssociationsSourceBehavior.m
+++ entourage/Classes/Utils/Behaviors/UITableView/Associations/OTAssociationsSourceBehavior.m
@@ -10,6 +10,14 @@
 #import "SVProgressHUD.h"
 #import "OTAssociationsService.h"
 #import "OTTableDataSourceBehavior.h"
+#import "OTAssociation.h"
+#import "OTConsts.h"
+
+@interface OTAssociationsSourceBehavior ()
+
+@property (nonatomic, strong) OTAssociation *originalAssociation;
+
+@end
 
 @implementation OTAssociationsSourceBehavior
 
@@ -18,10 +26,47 @@
     [[OTAssociationsService new] getAllAssociationsWithSuccess:^(NSArray *items) {
         [SVProgressHUD dismiss];
         [self updateItems:items];
+        [self storeOriginalSuppportedAssociation];
         [self.tableView reloadData];
     } failure:^(NSError *error) {
         [SVProgressHUD dismiss];
     }];
+}
+
+- (void)updateAssociation {
+    OTAssociation *currentAssociation = [self getCurrentAssociation];
+    if(currentAssociation == self.originalAssociation)
+        return;
+    [SVProgressHUD show];
+    if(self.originalAssociation) {
+        [[OTAssociationsService new] updateAssociation:self.originalAssociation isDefault:NO withSuccess:^(OTAssociation *updated) {
+            if(currentAssociation == nil)
+                [SVProgressHUD dismiss];
+            else
+                [[OTAssociationsService new] updateAssociation:currentAssociation isDefault:YES withSuccess:^(OTAssociation *nUpdated) {
+                    [SVProgressHUD dismiss];
+                } failure:^(NSError *error) {
+                    [SVProgressHUD showErrorWithStatus:OTLocalizedString(@"update_association_error")];
+                }];
+        } failure:^(NSError *error) {
+            [SVProgressHUD showErrorWithStatus:OTLocalizedString(@"update_association_error")];
+        }];
+    }
+}
+
+#pragma mark - private methods
+
+- (void)storeOriginalSuppportedAssociation {
+    for(OTAssociation *association in self.items)
+        if(association.isDefault) {
+            self.originalAssociation = association;
+            break;
+        }
+}
+
+- (OTAssociation *)getCurrentAssociation {
+    NSArray *defaultAssociations = [self.items filteredArrayUsingPredicate:[NSPredicate predicateWithFormat:@"isDefault==1"]];
+    return defaultAssociations.count > 0 ? defaultAssociations.firstObject : nil;
 }
 
 @end
