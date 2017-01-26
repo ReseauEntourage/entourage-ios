8a3fa656ce35794047fe42e760d22cde8ad6b85c
Sergiu Buceac <sergiu.buceac@tecknoworks.com>
2017-01-26 15:10:20 +0200

    EMA-992 fixed

--- entourage/Classes/Controller/ActiveFeedItem/ChatCells/OTJoinRequestedCell.m
+++ entourage/Classes/Controller/ActiveFeedItem/ChatCells/OTJoinRequestedCell.m
@@ -10,6 +10,7 @@
 #import "OTFeedItemJoiner.h"
 #import "UIButton+entourage.h"
 #import "OTTableDataSourceBehavior.h"
+#import "OTConsts.h"
 
 // Sergiu : again apologies for magic numbers (i hate ios tables with autolayout)
 #define BUTTON_MARGIN 144
@@ -34,6 +35,8 @@
     self.lblMessage.text = @"";
     if(joiner.message.length > 0)
         self.lblMessage.text = [NSString stringWithFormat:@"\"%@\"", joiner.message];
+    NSString *joinType = [joiner.feedItem isKindOfClass:[OTTour class]] ? @"want_to_join_tour" : @"want_to_join_entourage";
+    self.lblJoinType.text = OTLocalizedString(joinType);
 }
 
 - (IBAction)showUserDetails:(id)sender {
