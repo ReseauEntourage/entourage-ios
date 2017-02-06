2de06ce78edf3747de61a95beb46828c32cdc616
Sergiu Buceac <sergiu.buceac@tecknoworks.com>
2017-02-06 15:21:35 +0200

    EMA-1020 fixed by adding envoyer button back

---
+++ entourage/Classes/View/OTAutoGrowTextView.h
@@ -0,0 +1,16 @@
+//
+//  OTAutoGrowTextView.h
+//  entourage
+//
+//  Created by sergiu buceac on 2/6/17.
+//  Copyright © 2017 OCTO Technology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OTAutoGrowTextView : UITextView
+
+@property (nonatomic) CGFloat maxHeight;
+@property (nonatomic) CGFloat minHeight;
+
+@end
