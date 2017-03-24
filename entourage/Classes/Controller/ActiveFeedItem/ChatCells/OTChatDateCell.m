514178057ed85c9a64d4736d64b9d3a12b83e386
sergiu.buceac <sergiu.buceac@tecknoworks.com>
2017-03-24 14:36:32 +0200

    EMA-984 small refactor

--- entourage/Classes/Controller/ActiveFeedItem/ChatCells/OTChatDateCell.m
+++ entourage/Classes/Controller/ActiveFeedItem/ChatCells/OTChatDateCell.m
@@ -18,16 +18,16 @@
 #pragma mark - private methods
 
 - (NSString *)getDateString: (NSDate *) date {
+    if([[NSCalendar currentCalendar] isDateInToday:date])
+        return OTLocalizedString(@"today").uppercaseString;
+    if([[NSCalendar currentCalendar] isDateInYesterday:date])
+        return OTLocalizedString(@"yesterday").uppercaseString;
+
     NSDateComponents *components = [[NSCalendar currentCalendar] components:NSCalendarUnitYear|NSCalendarUnitMonth|NSCalendarUnitDay fromDate:date];
     NSInteger day = [components day];
     NSInteger month = [components month];
     NSInteger year = [components year];
     NSString *month_string = [NSString stringWithFormat:@"month_%ld", (long)month];
-    
-    if([[NSCalendar currentCalendar] isDateInToday:date])
-        return OTLocalizedString(@"today").uppercaseString;
-    if([[NSCalendar currentCalendar] isDateInYesterday:date])
-        return OTLocalizedString(@"yesterday").uppercaseString;
     return [NSString stringWithFormat:@"%ld %@ %ld", (long)day, OTLocalizedString(month_string).uppercaseString, (long) year] ;
 }
 
--- entourage/Classes/Utils/Services/OTMessagingService.m
+++ entourage/Classes/Utils/Services/OTMessagingService.m
@@ -17,53 +17,53 @@
         return;
     
     [dataSource.items removeAllObjects];
+    id<OTMessagingDelegate> messaging = [[OTFeedItemFactory createFor:feedItem] getMessaging];
+
+    NSMutableArray *allItems = [NSMutableArray new];
+    [allItems addObjectsFromArray:[messaging getTimelineStatusMessages]];
+    
     dispatch_group_t group = dispatch_group_create();
     
-    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^() {
-        id<OTMessagingDelegate> messaging = [[OTFeedItemFactory createFor:feedItem] getMessaging];
+    
+    dispatch_group_enter(group);
+    dispatch_group_enter(group);
+    dispatch_group_enter(group);
 
-        NSMutableArray *allItems = [NSMutableArray new];
-        [allItems addObjectsFromArray:[messaging getTimelineStatusMessages]];
-        
-        dispatch_group_enter(group);
-        [messaging getMessagesWithSuccess:^(NSArray *items) {
-            if([items count] > 0)
-                @synchronized (allItems) {
-                    [allItems addObjectsFromArray:items];
-                }
-            dispatch_group_leave(group);
-        } failure:^(NSError *error) {
-            dispatch_group_leave(group);
-        }];
-
-        dispatch_group_enter(group);
-        [messaging getFeedItemUsersWithStatus:nil success:^(NSArray *items) {
-            if([items count] > 0)
-                @synchronized (allItems) {
-                    for(OTFeedItemJoiner *joiner in items)
-                        joiner.feedItem = feedItem;
-                    [allItems addObjectsFromArray:items];
-                }
-            dispatch_group_leave(group);
-        } failure:^(NSError *error) {
-            dispatch_group_leave(group);
-        }];
-
-        dispatch_group_enter(group);
-        [messaging getEncountersWithSuccess:^(NSArray *items) {
-            if([items count] > 0)
-                @synchronized (allItems) {
-                    [allItems addObjectsFromArray:items];
-                }
-            dispatch_group_leave(group);
-        } failure:^(NSError *error) {
-            dispatch_group_leave(group);
-        }];
-        
-        dispatch_group_notify(group, dispatch_get_main_queue(), ^{
-            [self sendItems:allItems toSource:dataSource];
-        });
+    dispatch_group_notify(group, dispatch_get_main_queue(), ^{
+        [self sendItems:allItems toSource:dataSource];
     });
+
+    [messaging getMessagesWithSuccess:^(NSArray *items) {
+        if([items count] > 0)
+            @synchronized (allItems) {
+                [allItems addObjectsFromArray:items];
+            }
+        dispatch_group_leave(group);
+    } failure:^(NSError *error) {
+        dispatch_group_leave(group);
+    }];
+
+    [messaging getFeedItemUsersWithStatus:nil success:^(NSArray *items) {
+        if([items count] > 0)
+            @synchronized (allItems) {
+                for(OTFeedItemJoiner *joiner in items)
+                    joiner.feedItem = feedItem;
+                [allItems addObjectsFromArray:items];
+            }
+        dispatch_group_leave(group);
+    } failure:^(NSError *error) {
+        dispatch_group_leave(group);
+    }];
+
+    [messaging getEncountersWithSuccess:^(NSArray *items) {
+        if([items count] > 0)
+            @synchronized (allItems) {
+                [allItems addObjectsFromArray:items];
+            }
+        dispatch_group_leave(group);
+    } failure:^(NSError *error) {
+        dispatch_group_leave(group);
+    }];
 }
 
 #pragma mark - private methods
@@ -85,19 +85,15 @@
     NSMutableArray *result = [NSMutableArray new];
     NSDate *date = [NSDate dateWithTimeIntervalSince1970:0];
     for(OTFeedItemTimelinePoint *item in sortedItems){
-        if(![self isDay:item.date SameDayAs:date]){
+        if(![[NSCalendar currentCalendar] isDate:item.date inSameDayAsDate:date]) {
             date = item.date;
             OTFeedItemTimelinePoint *chatPoint = [OTFeedItemTimelinePoint new];
             chatPoint.date = date;
-           [result addObject:chatPoint];
+            [result addObject:chatPoint];
         }
         [result addObject:item];
     }
     return result;
-}
-
-- (BOOL)isDay: (NSDate *)date1 SameDayAs: (NSDate *) date2 {
-    return [[NSCalendar currentCalendar] isDate:date1 inSameDayAsDate:date2];
 }
 
 @end
