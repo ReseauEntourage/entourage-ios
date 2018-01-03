49c2dca1ba389ffda88b0ce98cafe75b9ce5c057
VeronicaGliga <veronica.gliga@tecknoworks.com>
2018-01-03 17:23:27 +0200

    check webview back arrow issue

--- entourage/Classes/Controller/OTWebViewController.m
+++ entourage/Classes/Controller/OTWebViewController.m
@@ -35,13 +35,13 @@
                                            byRoundingCorners: UIRectCornerTopLeft | UIRectCornerTopRight
                                                  cornerRadii: (CGSize){14, 14}].CGPath;
     self.animatedView.layer.mask = maskLayer;
-    UIPanGestureRecognizer *panGestureRecognizer = [[UIPanGestureRecognizer alloc]
-                                                    initWithTarget:self
-                                                    action:@selector(moveViewWithGestureRecognizer:)];
-    [self.view addGestureRecognizer:panGestureRecognizer];
-    UITapGestureRecognizer *tapGestureRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self
-                                                                                           action:@selector(tap:)];
-    [self.view addGestureRecognizer:tapGestureRecognizer];
+//    UIPanGestureRecognizer *panGestureRecognizer = [[UIPanGestureRecognizer alloc]
+//                                                    initWithTarget:self
+//                                                    action:@selector(moveViewWithGestureRecognizer:)];
+//    [self.view addGestureRecognizer:panGestureRecognizer];
+//    UITapGestureRecognizer *tapGestureRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self
+//                                                                                           action:@selector(tap:)];
+//    [self.view addGestureRecognizer:tapGestureRecognizer];
     _webView.delegate = self;
 }
 
@@ -72,13 +72,13 @@
 }
 
 - (void)configureUIBarButtonItems {
-    UIImage *menuImage = [[UIImage imageNamed:@"back.png"]
+    UIImage *backImage = [[UIImage imageNamed:@"back.png"]
                           imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
-    UIBarButtonItem *menuButton = [[UIBarButtonItem alloc] init];
-    [menuButton setImage:menuImage];
-    [menuButton setTarget:self];
-    [menuButton setAction:@selector(close)];
-    [self.navigationItem setLeftBarButtonItem:menuButton];
+    UIBarButtonItem *backButton = [[UIBarButtonItem alloc] init];
+    [backButton setImage:backImage];
+    [backButton setTarget:self];
+    [backButton setAction:@selector(closeWebview)];
+    [self.navigationItem setLeftBarButtonItem:backButton];
     
     NSMutableArray *rightButtons = [NSMutableArray new];
     UIButton *more = [UIButton buttonWithType:UIButtonTypeCustom];
@@ -116,7 +116,7 @@
     }
 }
 
-- (void)close {
+- (void)closeWebview {
     if(self.webView.canGoBack)
         [self.webView goBack];
     else {
@@ -174,40 +174,40 @@
     [self presentViewController:activityVC animated:YES completion:nil];
 }
 
-- (void)moveViewWithGestureRecognizer:(UIPanGestureRecognizer *)panGestureRecognizer{
-    [UIView animateWithDuration:0.5 animations:^{
-        CGPoint velocity = [panGestureRecognizer velocityInView:self.animatedView];
-        CGPoint translation = [panGestureRecognizer translationInView:self.animatedView];
-        CGFloat finalY = translation.y + 0.2*velocity.y;
-        if(translation.y > 0) {
-            if (finalY < DISTANCE_ABOVE_NAVIGATION_BAR) { 
-                finalY = DISTANCE_ABOVE_NAVIGATION_BAR;
-            } else if (finalY > self.view.frame.size.height) {
-                finalY = self.view.frame.size.height;
-            }
-            CGRect frame = self.animatedView.frame;
-            frame.origin.y = finalY;
-            self.animatedView.frame = frame;
-        }
-    }];
-    if(panGestureRecognizer.state == UIGestureRecognizerStateEnded){
-        [UIView animateWithDuration:0.5 animations:^{
-            CGPoint translation = [panGestureRecognizer translationInView:self.animatedView];
-            if(translation.y >= self.view.bounds.size.height/2)
-                [self close];
-            else {
-                CGRect frame = self.animatedView.frame;
-                frame.origin = CGPointMake(0, DISTANCE_ABOVE_NAVIGATION_BAR);
-                self.animatedView.frame = frame;
-            }
-        }];
-    }
-}
-
-- (void)tap:(UITapGestureRecognizer *)tapGestureRecognizer {
-    CGPoint touchPoint = [tapGestureRecognizer locationInView:self.view];
-    if(touchPoint.y < DISTANCE_ABOVE_NAVIGATION_BAR)
-        [self close];
-}
+//- (void)moveViewWithGestureRecognizer:(UIPanGestureRecognizer *)panGestureRecognizer{
+//    [UIView animateWithDuration:0.5 animations:^{
+//        CGPoint velocity = [panGestureRecognizer velocityInView:self.animatedView];
+//        CGPoint translation = [panGestureRecognizer translationInView:self.animatedView];
+//        CGFloat finalY = translation.y + 0.2*velocity.y;
+//        if(translation.y > 0) {
+//            if (finalY < DISTANCE_ABOVE_NAVIGATION_BAR) { 
+//                finalY = DISTANCE_ABOVE_NAVIGATION_BAR;
+//            } else if (finalY > self.view.frame.size.height) {
+//                finalY = self.view.frame.size.height;
+//            }
+//            CGRect frame = self.animatedView.frame;
+//            frame.origin.y = finalY;
+//            self.animatedView.frame = frame;
+//        }
+//    }];
+//    if(panGestureRecognizer.state == UIGestureRecognizerStateEnded){
+//        [UIView animateWithDuration:0.5 animations:^{
+//            CGPoint translation = [panGestureRecognizer translationInView:self.animatedView];
+//            if(translation.y >= self.view.bounds.size.height/2)
+//                [self closeWebview];
+//            else {
+//                CGRect frame = self.animatedView.frame;
+//                frame.origin = CGPointMake(0, DISTANCE_ABOVE_NAVIGATION_BAR);
+//                self.animatedView.frame = frame;
+//            }
+//        }];
+//    }
+//}
+//
+//- (void)tap:(UITapGestureRecognizer *)tapGestureRecognizer {
+//    CGPoint touchPoint = [tapGestureRecognizer locationInView:self.view];
+//    if(touchPoint.y < DISTANCE_ABOVE_NAVIGATION_BAR)
+//        [self closeWebview];
+//}
 
 @end
