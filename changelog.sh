4383a8553b0f492708a579a0e2b152385eb0fae6
François Pellissier <francois.pellissier@gmail.com>
2018-01-19 18:32:38 +0100

    chore(release) add automatic changelog

--- changelog.sh
+++ changelog.sh
@@ -18,7 +18,7 @@ else
     build=`git rev-list HEAD --count`
     previous_tag="$(git describe --abbrev=0 --tags $(git rev-list --tags --skip=1 --max-count=1))"
 
-    previous_tag="Version_4.1.722"
+    previous_tag="Version_4.2.769"
 
     filename="GITCHANGELOG-FROM-$previous_tag-TO-$latest_tag.md"
     echo "#Changelog" > $filename
