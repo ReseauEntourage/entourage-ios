b5bf9b61bf789607d2c90fdced3fe5b9c2e0d47a
Lazar Sidor <lazar.sidor@gmail.com>
2018-04-17 16:12:31 +0300

    Updated bridging headers to support separate inclusion for each target

---
+++ entourage/Settings/Environment/Entourage-Bridging-Header.h
@@ -0,0 +1,7 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "Shared-Bridging-Header.h"
+#import "OTAmazonConsts.h"
+#import "OTMixpanelConsts.h"
