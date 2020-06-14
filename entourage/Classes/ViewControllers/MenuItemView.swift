2a340591645841db4937d2ed3a7e70c8380f63f4
amseddi <amseddi@users.noreply.github.com>
2020-06-14 13:31:50 +0200

    removed proxy variables

--- entourage/Classes/ViewControllers/MenuItemView.swift
+++ entourage/Classes/ViewControllers/MenuItemView.swift
@@ -8,46 +8,31 @@ final class MenuItemView: UIView {
 
     static func header(title: String) -> MenuItemView {
         MenuItemView(withSeparator: false)
-            |> set(\MenuItemView.font, UIFont.SFUIText(size: 15, type: .medium))
-            <> set(\MenuItemView.textColor, UIColor.appGreyishBrown())
-            <> set(\MenuItemView.text, title.uppercased())
+            |> set(\MenuItemView.label.font, UIFont.SFUIText(size: 15, type: .medium))
+            <> set(\MenuItemView.label.textColor, UIColor.appGreyishBrown())
+            <> set(\MenuItemView.label.text, title.uppercased())
     }
 
     static func secondary(title: String, addSeparator: Bool) -> MenuItemView {
         MenuItemView(withSeparator: addSeparator)
             |> Style.View.whiteBackground()
-            <> set(\MenuItemView.font, UIFont.SFUIText(size: 15, type: .light))
-            <> set(\MenuItemView.textColor, UIColor.appGreyishBrown())
-            <> set(\MenuItemView.text, title)
+            <> set(\MenuItemView.label.font, UIFont.SFUIText(size: 15, type: .light))
+            <> set(\MenuItemView.label.textColor, UIColor.appGreyishBrown())
+            <> set(\MenuItemView.label.text, title)
     }
 
     static func main(title: String) -> MenuItemView {
         MenuItemView(withSeparator: false)
             |> Style.View.orangeBackground()
-            <> set(\MenuItemView.font, UIFont.SFUIText(size: 15, type: .light))
-            <> set(\MenuItemView.textColor, UIColor.white)
-            <> set(\MenuItemView.text, title)
+            <> set(\MenuItemView.label.font, UIFont.SFUIText(size: 15, type: .light))
+            <> set(\MenuItemView.label.textColor, UIColor.white)
+            <> set(\MenuItemView.label.text, title)
     }
 
     private enum Constants {
         static let height: CGFloat = 45
         static let separatorHeight: CGFloat = 1
         static let padding: CGFloat = 14
-    }
-
-    private var font: UIFont {
-        get { label.font }
-        set { label.font = newValue }
-    }
-
-    private var textColor: UIColor {
-        get { label.textColor }
-        set { label.textColor = newValue }
-    }
-
-    private var text: String? {
-        get { label.text }
-        set { label.text = newValue }
     }
 
     private let label = UILabel()
