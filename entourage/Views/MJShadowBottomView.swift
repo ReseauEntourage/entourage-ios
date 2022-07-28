5a9c309d7bfb3624630773c8d04528979dcda444
Jr <jerome@mj-devs.fr>
2022-07-28 16:22:05 +0200

    Fix shadow cell Feed neighborhood/event

--- entourage/Views/MJShadowBottomView.swift
+++ entourage/Views/MJShadowBottomView.swift
@@ -9,11 +9,7 @@ import Foundation
 
 class MJShadowBottomView: UIView {
     
-    var setupShadowDone: Bool = false
-    
     public func setupShadow() {
-        if setupShadowDone { return }
-        
         self.layer.cornerRadius = ApplicationTheme.bigCornerRadius
         self.layer.shadowOffset = CGSize(width: 0, height: 3)
         self.layer.shadowRadius = 3
@@ -22,8 +18,6 @@ class MJShadowBottomView: UIView {
         self.layer.shadowPath = UIBezierPath(roundedRect: self.bounds, byRoundingCorners: .allCorners, cornerRadii: CGSize(width: ApplicationTheme.bigCornerRadius, height: ApplicationTheme.bigCornerRadius)).cgPath
         self.layer.shouldRasterize = true
         self.layer.rasterizationScale = UIScreen.main.scale
-        
-        setupShadowDone = true
     }
     
     override func layoutSubviews() {
