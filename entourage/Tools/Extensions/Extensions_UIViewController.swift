827392d5a379a4f0a63433929fdb63e8af77cebb
Clément Perrousset <clement.perrousset@gmail.com>
2023-07-06 17:59:47 +0200

    fix(deeplink): fix some bad throw  EN-6282

--- entourage/Tools/Extensions/Extensions_UIViewController.swift
+++ entourage/Tools/Extensions/Extensions_UIViewController.swift
@@ -118,3 +118,24 @@ extension UIApplication {
      return self.keyWindow?.rootViewController?.topFrontViewController
  }
 }
+
+
+extension UIViewController {
+    var isModal: Bool {
+        if let navigationController = self.navigationController {
+            if navigationController.viewControllers.first != self {
+                return false
+            }
+        }
+        if self.presentingViewController != nil {
+            return true
+        }
+        if self.navigationController?.presentingViewController?.presentedViewController == self.navigationController  {
+            return true
+        }
+        if self.tabBarController?.presentingViewController is UITabBarController {
+            return true
+        }
+        return false
+    }
+}
