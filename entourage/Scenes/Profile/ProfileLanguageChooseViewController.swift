4311bce4e84b1fb139981f48d3f48d586bc8e520
Clément Perrousset <clement.perrousset@gmail.com>
2023-10-31 14:18:26 +0100

    chore(ProfileLangueChooseViewController): remove arabic for now

--- entourage/Scenes/Profile/ProfileLanguageChooseViewController.swift
+++ entourage/Scenes/Profile/ProfileLanguageChooseViewController.swift
@@ -43,7 +43,7 @@ class ProfileLanguageChooseViewController:UIViewController{
     func fillDTO(){
         tableDTO.removeAll()
         let preferredLanguage = LanguageManager.loadLanguageFromPreferences()
-        let languages = ["fr", "en", "ar", "es", "uk", "de", "ro", "pl"]
+        let languages = ["fr", "en", "es", "uk", "de", "ro", "pl"]
         for lang in languages {
             let isSelected = lang == preferredLanguage
             tableDTO.append(.languageCell(lang: lang, isSelected: isSelected))
