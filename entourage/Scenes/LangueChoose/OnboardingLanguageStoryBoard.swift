f92f781e5cb6a6b6e68a7dbf28112e070954da22
Clément Perrousset <clement.perrousset@gmail.com>
2024-09-17 16:01:07 +0200

    feature(ArabicVersionIOS) : arabic version

--- entourage/Scenes/LangueChoose/OnboardingLanguageStoryBoard.swift
+++ entourage/Scenes/LangueChoose/OnboardingLanguageStoryBoard.swift
@@ -43,7 +43,7 @@ class OnboardingLanguageStoryBoard:UIViewController{
             LanguageManager.saveLanguageToPreferences(languageCode: preferredLanguage)
         }
 
-        let languages = ["fr", "en","uk", "es", "de", "ro", "pl"]
+        let languages = ["fr", "en","uk", "es", "de", "ro", "pl", "ar"]
         for lang in languages {
             let isSelected = lang == preferredLanguage
             tableDTO.append(.languageCell(lang: lang, isSelected: isSelected))
--- entourage/Scenes/Profile/ProfileLanguageChooseViewController.swift
+++ entourage/Scenes/Profile/ProfileLanguageChooseViewController.swift
@@ -44,7 +44,7 @@ class ProfileLanguageChooseViewController:UIViewController{
     func fillDTO(){
         tableDTO.removeAll()
         let preferredLanguage = LanguageManager.loadLanguageFromPreferences()
-        let languages = ["fr", "en", "uk", "es", "de", "ro", "pl"]
+        let languages = ["fr", "en", "uk", "es", "de", "ro", "pl","ar"]
         for lang in languages {
             let isSelected = lang == preferredLanguage
             tableDTO.append(.languageCell(lang: lang, isSelected: isSelected))
