41fa6fda1a11389df938ae8ba350fd7830d5519f
Clément Perrousset <clement.perrousset@gmail.com>
2024-10-16 17:44:57 +0200

    fix(ActionEditMainViewController) : fix some bug on edit from new creation

--- entourage/Scenes/HomeV2/HomeV2ViewController.swift
+++ entourage/Scenes/HomeV2/HomeV2ViewController.swift
@@ -114,7 +114,6 @@ class HomeV2ViewController: UIViewController {
         if let bundleIdentifier = Bundle.main.bundleIdentifier {
             print("Bundle Identifier: \(bundleIdentifier)")
         }
-        print("eho home")
         getUserInfo()
     }
     
@@ -252,7 +251,6 @@ class HomeV2ViewController: UIViewController {
                     
                     // Incrémenter le nombre de connexions
                     let connectionCount = self.incrementConnectionCount()
-                    print("eho connection count : " , connectionCount)
                     // Vérifier si c'est la 2ème ou la 10ème connexion
                     if connectionCount == 1 || connectionCount == 4 || connectionCount == 9 {
                         self.presentNotificationDemandViewController()
