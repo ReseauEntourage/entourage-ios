7ea73389cb22feb96bae18c8d724cbd336444bed
Clément Perrousset <clement.perrousset@gmail.com>
2024-11-27 09:11:45 +0100

    fix(NotificationDemanViewController) : fixed notif popup

--- entourage/Scenes/Onboarding/NotificationDemandViewController.swift
+++ entourage/Scenes/Onboarding/NotificationDemandViewController.swift
@@ -11,17 +11,18 @@ class NotificationDemandViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         ui_title.text = "notif_demand_title".localized
         ui_subtitle.text = "notif_demand_subtitle".localized
         configureOrangeButton(ui_btn_accept_notif, withTitle: "notif_demand_btn_accept".localized)
         configureWhiteButton(ui_btn_disable_notif, withTitle: "notif_demand_btn_refuse".localized)
         
-        // Vérifiez si les notifications ont déjà été autorisées
-        checkNotificationAuthorization()
-        
         // Ajoutez les actions aux boutons
         ui_btn_accept_notif.addTarget(self, action: #selector(didTapAcceptNotif), for: .touchUpInside)
         ui_btn_disable_notif.addTarget(self, action: #selector(didTapDisableNotif), for: .touchUpInside)
+        
+        // Vérifiez si les notifications sont déjà autorisées
+        checkNotificationAuthorization()
     }
     
     func configureOrangeButton(_ button: UIButton, withTitle title: String) {
@@ -45,14 +46,14 @@ class NotificationDemandViewController: UIViewController {
     }
     
     func checkNotificationAuthorization() {
-        UNUserNotificationCenter.current().getNotificationSettings { (settings) in
+        UNUserNotificationCenter.current().getNotificationSettings { [weak self] settings in
             DispatchQueue.main.async {
                 switch settings.authorizationStatus {
                 case .authorized, .provisional:
-                    self.goHomeMain()
+                    // Notifications déjà autorisées, ferme la vue
+                    self?.goHomeMain()
                 case .denied, .notDetermined:
-                    // Ne rien faire, l'utilisateur doit choisir
-                    self.goHomeMain()
+                    // L'utilisateur doit choisir, ne rien faire
                     break
                 @unknown default:
                     break
@@ -62,21 +63,23 @@ class NotificationDemandViewController: UIViewController {
     }
     
     @objc func didTapAcceptNotif() {
-        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
-            if granted {
-                DispatchQueue.main.async {
-                    self.goHomeMain()
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { [weak self] granted, error in
+            DispatchQueue.main.async {
+                if granted {
+                    // Autorisation accordée, ferme la vue
+                    UIApplication.shared.registerForRemoteNotifications()
+                    self?.goHomeMain()
+                } else {
+                    // Autorisation refusée, reste sur l'écran
+                    print("Notifications denied: \(String(describing: error))")
                 }
-            } else {
-                self.goHomeMain()
             }
         }
     }
 
     @objc func didTapDisableNotif() {
-        // Gérer le refus ici si nécessaire
-        self.dismiss(animated: true) {
-        }
+        // L'utilisateur refuse explicitement les notifications
+        self.dismiss(animated: true, completion: nil)
     }
     
     func goHomeMain() {
