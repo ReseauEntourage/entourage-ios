793111de8cbf33860a6041dc0cb2d58c5a84f43f
clem <clement.perrousset@gmail.com>
2026-02-02 14:22:10 +0100

    fix(EnhancedOnboardingBUttonCell ) : changed wording

--- entourage/Scenes/enhancedOnboarding/cells/EnahancedOnboardingButtonCell.swift
+++ entourage/Scenes/enhancedOnboarding/cells/EnahancedOnboardingButtonCell.swift
@@ -39,7 +39,7 @@ class EnahancedOnboardingButtonCell:UITableViewCell{
     
     func configureForMainFilter(){
         self.configure()
-        self.ui_btn_next.setTitle("btn_main_filter_validate_title".localized, for: .normal)
+        self.ui_btn_next.setTitle("validate".localized, for: .normal)
         self.ui_btn_configure_later.setTitle("cancel".localized, for: .normal)
     }
     
