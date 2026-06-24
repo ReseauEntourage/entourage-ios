a9297a1af90c6d48cfe3e65e66de4fd0aef45902
clem <clement.perrousset@gmail.com>
2026-06-24 11:39:26 +0200

    fix(Home.swift) : fixed call for welcome home

--- entourage/Models/Home.swift
+++ entourage/Models/Home.swift
@@ -26,7 +26,6 @@ struct UserHome:Codable {
 
     
     var congratulations = HomeActions()
-    var badges: [String] = []
 
     var displayName:String {
         get {
@@ -53,7 +52,6 @@ struct UserHome:Codable {
         case events = "events"
         case signablePermission = "signable_permission"
         case association
-        case badges
     }
 }
 
